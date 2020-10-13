67b8653de7c123a61fe15ad2d1711f2a8d3b11e9
Gh057ly <45234225+Gh057ly@users.noreply.github.com>
2020-10-13 14:34:59 +0200

    Sat article elementer ind i html ved brug af overskrivning Billederne virker bruh

--- Knox/src/main/webapp/resources/javascript/convert-to-html-nordjysk.js
+++ Knox/src/main/webapp/resources/javascript/convert-to-html-nordjysk.js
@@ -5,10 +5,29 @@ function converttohtml(input){
 
     console.log("this is the converter");
     console.log(article.articletext)
-    document.getElementById("p1").innerHTML = article.title;
-    document.getElementById("p2").innerHTML = article.subtitle;
-    document.getElementById("p3").innerHTML = article.author;
-    document.getElementById("p4").innerHTML = article.date;
-    document.getElementById("p5").innerHTML = article.articletext;
+    document.getElementById("Title").innerHTML = article.title;
+    document.getElementById("SubTitle").innerHTML = article.subtitle;
+    document.getElementById("Author").innerHTML = article.author;
+    document.getElementById("Date").innerHTML = article.date;
+    document.getElementById("ArticleText").innerHTML = article.articleText;
 
+
+
+    for(var i=0; i<article.images.length; i++){
+        var para = document.createElement("img");
+        para.src = article.images[i];
+        var element = document.getElementById("Pictures");
+        element.appendChild(para);
+    }
+
+
+
+/*
+    document.getElementById("Picture0").src= article.images[0];
+    document.getElementById("Picture1").src= article.images[1];
+    document.getElementById("Picture2").src= article.images[2];
+    document.getElementById("Picture3").src= article.images[3];
+    document.getElementById("Picture4").src= article.images[4];
+
+*/
 }
