b77ed3b3748a5b9bd6b5cc512c16eabb0e6c0393
bjarked <bjarked47@gmail.com>
2020-11-19 13:10:39 +0100

    Function Renaming

--- Knox/src/main/java/knox/frontend/controllers/HomeController.java
+++ Knox/src/main/java/knox/frontend/controllers/HomeController.java
@@ -9,14 +9,14 @@ import org.springframework.web.bind.annotation.*;
 public class HomeController {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String printHallo(ModelMap model){
+    public String GetIndexPage(ModelMap model){
 
         model.addAttribute("message","KNOX");
         return "index";
     }
 
     @RequestMapping("/test")
-    public String testView(ModelMap model){
+    public String GetTestingPage(ModelMap model){
         model.addAttribute("message","Hallo Spring MVC Framework");
         FileManager fileManager = new FileManager(model);
         fileManager.AddCssFile("test");
