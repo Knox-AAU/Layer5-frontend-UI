189e03d3b4ab8ba6cd8f31610e2ba580920e7559
bjarked <bjarked47@gmail.com>
2020-12-20 20:23:39 +0100

    Fix: Login theme CSS missing

--- Knox/src/main/java/knox/frontend/controller/HomeController.java
+++ Knox/src/main/java/knox/frontend/controller/HomeController.java
@@ -52,7 +52,7 @@ public class HomeController {
     @RequestMapping(value = "/login", method = RequestMethod.GET) // This is accessed, when the Url is entered
     public String Login(ModelMap model){
         FileManager fileManager = new FileManager(model);
-        fileManager.AddCssFile("login");
+        fileManager.AddCssFile("knox").AddCssFile("login");
         fileManager.finish();
 
         return "Neutral/LoginPage";
