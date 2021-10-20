d304f6cbe9f19bc0eb795e8f74e4fa7d887aafa0
adroob19 <62326343+adroob19@users.noreply.github.com>
2021-10-20 11:51:34 +0200

    Added a getpdf controller for recieving and returning pdfs

---
+++ Knox/src/main/java/knox/frontend/controller/GetpdfController.java
@@ -0,0 +1,37 @@
+package knox.frontend.controller;
+
+import knox.frontend.model.UserData;
+import knox.frontend.model.Search;
+import knox.frontend.utility.FileManager;
+import knox.frontend.utility.GrundfosConnection;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
+
+
+public class GetpdfController {
+
+    RestTemplate restTemplate = new RestTemplate();
+    @RequestMapping(value="/getpdf", method=RequestMethod.GET)
+    public ResponseEntity<byte[]> getPDF(@RequestParam int id) {
+
+
+        String url = "localhost:8081/api/file?id=" + id;
+        ResponseEntity<byte[]> response = this.restTemplate.getForEntity(url, byte[].class);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType("application/pdf"));
+        String filename = "pdf" + id + ".pdf";
+        headers.add("Content-Disposition", "inline; filename=" + filename);
+        headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
+
+        return response;
+    }
+}
